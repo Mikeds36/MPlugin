834408abf9a5c733b3fdb06ea56e81228a7e201b
dora <55905774+dora-0@users.noreply.github.com>
2019-10-24 11:43:30 +0900

    /notify 명령어 구현 (#13)
    * Add NotifyCmd
    
    * Add /notify
    
    * Add: players, playerTime
    
    * Test: print PlayerTime of sender
    
    * Add: command /notify
    
    * Add: refreshTime(), printTime()
    
    * Fix: move 'players' to refreshTime()
    
    * Fix: getPlayerTime() -> getStatistic()
    
    getPlayerTime()은 월드의 시간을 가져오는 메소드, 플레이 시간은 getStatistic(Statistic.PLAY_ONE_MINUTE)를 사용해 가져옴
    
    * fix typo
    
    * Refactor: NotifyCmd, PlayTimeManager
    
    i) NotifyCmd 클래스를 test.minecraft.mplugin.commands.notify 패키지로 이동
    ii) 플레이어의 플레이 시간을 받아오는 메소드를 PlayTimeManager 클래스로 분리
    
    * Add: GameEventListener
    
    PlayerJoinEvent 추가 : 서버 입장 시 환영 메시지 출력, 플레이 시간 계산
    
    * Add: TaskManager, /notifyall
    
    i) /notifyall 명령어 추가
    모든 플레이어에게 플레이 시간을 알려준다. (OP 전용)
    
    ii) TaskManager 추가
    버킷 스케줄 지정에 들어가는 Runnable 파라미터가 코드를 길어지게 하므로, 따로 TaskManager로 분리해서 관리
    
    * Fix: Make final
    
    * Fix: /notifyall 명령어 사용 시 Usage 출력 문제
    
    + /notifyall 명령어 사용 시 글자 색이 초록색으로 보이지 않는 문제

--- src/main/java/test/minecraft/mplugin/Main.java
+++ src/main/java/test/minecraft/mplugin/Main.java
@@ -5,7 +5,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import test.minecraft.mplugin.commands.GamemodeCmd;
 import test.minecraft.mplugin.commands.HelloCmd;
+import test.minecraft.mplugin.commands.notify.NotifyCmd;
+import test.minecraft.mplugin.core.GameEventListener;
 import test.minecraft.mplugin.core.ConstructTabCompleter;
+import test.minecraft.mplugin.core.TaskManager;
 
 import java.util.Objects;
 
@@ -15,10 +18,18 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         // Hello, World!
+        TaskManager taskMgr = TaskManager.getInstance();
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, taskMgr.get("notify_refresh"), 0L, 1200L);
+
+        getServer().getPluginManager().registerEvents(new GameEventListener.PlayerJoin(this), this);
         Objects.requireNonNull(getCommand("hello")).setExecutor(new HelloCmd(this));
         Objects.requireNonNull(getCommand("gm")).setExecutor(new GamemodeCmd(this));
         Objects.requireNonNull(getCommand("gm")).setTabCompleter(new ConstructTabCompleter.Gamemode());
-        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "Hello, World!");
+        Objects.requireNonNull(getCommand("notify")).setExecutor(new NotifyCmd(this));
+        Objects.requireNonNull(getCommand("notifyall")).setExecutor(new NotifyCmd.BroadcastAll());
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "[" + this.getName() + "] " + "Version " + this.getDescription().getVersion());
     }
 
     @Override
---
+++ src/main/java/test/minecraft/mplugin/core/TaskManager.java
@@ -0,0 +1,43 @@
+package test.minecraft.mplugin.core;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class TaskManager {
+    private final Map<String, Runnable> task = new HashMap<>();
+
+    private TaskManager() {
+        PlayTimeManager playTime = PlayTimeManager.getInstance();
+
+        this.set("notifyAll", () -> {
+            Set<Player> players = new HashSet<>(Bukkit.getOnlinePlayers());
+            for (Player p : players) {
+                p.sendMessage(ChatColor.GREEN + playTime.print(p));
+            }
+        });
+
+        this.set("notify_refresh", playTime::refresh);
+    }
+
+    private static class LazyHolder {
+        static final TaskManager INSTANCE = new TaskManager();
+    }
+
+    public static TaskManager getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private void set(String key, Runnable task) {
+        this.task.put(key, task);
+    }
+
+    public Runnable get(String key) {
+        return this.task.get(key);
+    }
+}
